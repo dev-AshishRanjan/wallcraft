b08f87f9ee47d48ebb246679fcd71224420a806c
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2025-12-30 19:19:40 +0530

    fix: command-menu alingment

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -62,7 +62,7 @@ export function CommandMenu({ themes, categories }: CommandMenuProps) {
       >
         <span className="hidden lg:inline-flex">Search wallpapers...</span>
         <span className="inline-flex lg:hidden">Search...</span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-auto hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
           <span className="text-xs">⌘</span>K
         </kbd>
       </Button>
